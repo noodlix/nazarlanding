33ebadb293dd55a4caa37c585b29851d4cb722d1
noodlix <zhibeck271205@gmail.com>
2024-03-26 10:00:06 +0500

    feat: imrpove youtube video styles

--- src/components/ui/video.tsx
+++ src/components/ui/video.tsx
@@ -3,18 +3,17 @@ import React from "react";
 
 export const Video = () => {
   return (
-    <div className="h-[30rem] md:h-[50rem] w-full flex items-center justify-center relative">
-      <div className="h-[20rem] md:h-[30rem] relative w-full flex items-center justify-center">
-        <iframe
-          width="90%"
-          height="100%"
-          src="https://www.youtube.com/embed/gZWNBIaZ0Jc?si=Bbf4Nob9YMcJJjfr"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
-      </div>
+    <div className="mb-24">
+      <h1 className="text-center text-3xl font-extrabold mb-6">demo</h1>
+      <iframe
+        width="540px"
+        height="360px"
+        src="https://www.youtube.com/embed/gZWNBIaZ0Jc?si=Bbf4Nob9YMcJJjfr"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 };
